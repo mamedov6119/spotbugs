d956815296b8e990fe97c8353d93532a76746c7c
Judit Knoll <judit.knoll@sigmatechnology.com>
2024-01-05 16:03:15 +0100

    add missing needed classes to perform the analysis on

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/AvoidClientSideLockingTest.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/AvoidClientSideLockingTest.java
@@ -15,7 +15,8 @@ public class AvoidClientSideLockingTest extends AbstractIntegrationTest {
     @Test
     void testBadClientSideLocking() {
         performAnalysis("avoidClientSideLocking/BadClientSideLockingBook.class", "avoidClientSideLocking/PrintableIPAddressList.class",
-                "avoidClientSideLocking/DataUpdater.class", "avoidClientSideLocking/Book.class");
+                "avoidClientSideLocking/DataUpdater.class", "avoidClientSideLocking/Book.class",
+                "avoidClientSideLocking/BadClientSideLockingIP.class", "avoidClientSideLocking/BadClientSideLockingMap.class");
 
         assertNumOfACSLBugs(3);
 
@@ -28,7 +29,8 @@ public class AvoidClientSideLockingTest extends AbstractIntegrationTest {
     @Test
     void testGoodEndOfFileChecks() {
         performAnalysis("avoidClientSideLocking/GoodClientSideLockingBook.class", "avoidClientSideLocking/GoodClientSideLockingIP.class",
-                "avoidClientSideLocking/DataUpdaterr.class", "avoidClientSideLocking/Book.class");
+                "avoidClientSideLocking/DataUpdaterr.class", "avoidClientSideLocking/Book.class", "avoidClientSideLocking/IPAddressList.class",
+                "avoidClientSideLocking/GoodClientSideLockingMap.class");
 
         assertNumOfACSLBugs(0);
     }
